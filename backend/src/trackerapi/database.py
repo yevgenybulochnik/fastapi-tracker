9595efd1b49e424afde31763b9faa0bbd090c469
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-08-23 18:22:25 -0500

    Use SQLALCHEMY_DATABASE_URI from config.settings

--- backend/src/trackerapi/database.py
+++ backend/src/trackerapi/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+from trackerapi.core.config import settings
+
+SQLALCHEMY_DATABASE_URI = settings.SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
