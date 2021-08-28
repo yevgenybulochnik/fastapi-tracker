38cf92501f493a1469ec76507554dff69d6b7298
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-08-28 17:41:18 -0500

    Run black over config tests

--- backend/tests/core/test_config.py
+++ backend/tests/core/test_config.py
@@ -3,45 +3,45 @@ from trackerapi.core.config import Settings
 
 
 def test_config_settings_default():
-    os.unsetenv('POSTGRES_SERVER')
-    os.unsetenv('POSTGRES_USER')
-    os.unsetenv('POSTGRES_PASSWORD')
-    os.unsetenv('POSTGRES_DB')
-    os.unsetenv('SQLALCHEMY_DATABASE_URI')
+    os.unsetenv("POSTGRES_SERVER")
+    os.unsetenv("POSTGRES_USER")
+    os.unsetenv("POSTGRES_PASSWORD")
+    os.unsetenv("POSTGRES_DB")
+    os.unsetenv("SQLALCHEMY_DATABASE_URI")
 
     settings = Settings()
 
-    assert settings.SQLALCHEMY_DATABASE_URI == 'sqlite:///./db.sqlite'
+    assert settings.SQLALCHEMY_DATABASE_URI == "sqlite:///./db.sqlite"
 
 
 def test_config_settings_missing():
-    os.unsetenv('POSTGRES_SERVER')
-    os.unsetenv('POSTGRES_USER')
-    os.unsetenv('POSTGRES_PASSWORD')
-    os.unsetenv('POSTGRES_DB')
-    os.unsetenv('SQLALCHEMY_DATABASE_URI')
+    os.unsetenv("POSTGRES_SERVER")
+    os.unsetenv("POSTGRES_USER")
+    os.unsetenv("POSTGRES_PASSWORD")
+    os.unsetenv("POSTGRES_DB")
+    os.unsetenv("SQLALCHEMY_DATABASE_URI")
 
-    os.environ['POSTGRES_USER'] = 'testuser'
+    os.environ["POSTGRES_USER"] = "testuser"
 
     settings = Settings()
 
-    assert settings.SQLALCHEMY_DATABASE_URI == 'sqlite:///./db.sqlite'
+    assert settings.SQLALCHEMY_DATABASE_URI == "sqlite:///./db.sqlite"
 
 
 def test_config_settings_postgres():
-    os.unsetenv('POSTGRES_SERVER')
-    os.unsetenv('POSTGRES_USER')
-    os.unsetenv('POSTGRES_PASSWORD')
-    os.unsetenv('POSTGRES_DB')
-    os.unsetenv('SQLALCHEMY_DATABASE_URI')
+    os.unsetenv("POSTGRES_SERVER")
+    os.unsetenv("POSTGRES_USER")
+    os.unsetenv("POSTGRES_PASSWORD")
+    os.unsetenv("POSTGRES_DB")
+    os.unsetenv("SQLALCHEMY_DATABASE_URI")
 
-    os.environ['POSTGRES_SERVER'] = 'db'
-    os.environ['POSTGRES_USER'] = 'postgres'
-    os.environ['POSTGRES_PASSWORD'] = 'postgres'
-    os.environ['POSTGRES_DB'] = 'testdb'
+    os.environ["POSTGRES_SERVER"] = "db"
+    os.environ["POSTGRES_USER"] = "postgres"
+    os.environ["POSTGRES_PASSWORD"] = "postgres"
+    os.environ["POSTGRES_DB"] = "testdb"
 
     settings = Settings()
 
     URI = settings.SQLALCHEMY_DATABASE_URI
 
-    assert URI == 'postgresql://postgres:postgres@db/testdb'
+    assert URI == "postgresql://postgres:postgres@db/testdb"
